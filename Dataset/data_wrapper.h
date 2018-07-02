b1fc64245b809ea661e0e3a595564b4ee81b2ed5
Sleepy <sleepysoft@gmail.com>
2018-07-02 16:33:37 +0800

    Refactor the project structure and fix warning.
    Change-Id: I5f8bedc2f2f347a6afe501b38552537ac24bd73d

--- Dataset/data_wrapper.h
+++ Dataset/data_wrapper.h
@@ -1,123 +1,129 @@
 #ifndef DATA_WRAPPER_H_
 #define DATA_WRAPPER_H_
 
+#pragma warning(push, 0)
 #include <string>
 #include <typeinfo>
 #include <stdint.h>
+#pragma warning(pop)
+
+#pragma warning(disable: 4820 4514 4710)
 
 namespace dw {
 
-	class data_wrapper
-	{
-	protected:
-		class placeholder
-		{
-		public:
-			placeholder() { }
-			virtual ~placeholder() { }
+    class data_wrapper
+    {
+    protected:
+        class placeholder
+        {
+        public:
+            placeholder() { }
+            virtual ~placeholder() { }
 
-			virtual const std::type_info & type_info() const = 0;
-			virtual placeholder* clone() const = 0;
-		};
+            virtual const std::type_info & type_info() const = 0;
+            virtual placeholder* clone() const = 0;
+        };
 
-		template< typename T >
-		class data_holder : public placeholder
-		{
-		public:
-			T value;
-		public:
-			data_holder(const T& val) : value(val)
-			{
+        template< typename T >
+        class data_holder : public placeholder
+        {
+        public:
+            T value;
+        public:
+            data_holder(const T& val) : value(val)
+            {
 
-			}
-			virtual ~data_holder()
-			{
+            }
+            virtual ~data_holder()
+            {
 
-			}
-			virtual const std::type_info & type_info() const
-			{
-				return typeid(T);
-			}
-			virtual placeholder* clone() const
-			{
-				return new data_holder(value);
-			}
-		};
+            }
+            virtual const std::type_info & type_info() const
+            {
+                return typeid(T);
+            }
+            virtual placeholder* clone() const
+            {
+                return new data_holder(value);
+            }
+        };
 
-	protected:
-		placeholder* content;
-		//const char* _content;
-	public:
-		data_wrapper() : content(NULL)
-		{
+    protected:
+        placeholder* content;
+        //const char* _content;
+    public:
+        data_wrapper() : content(NULL)
+        {
 
-		}
-		template< typename T >
-		data_wrapper(const T& val) : content(new data_holder< T >(val))
-		{
+        }
+        template< typename T >
+        data_wrapper(const T& val) : content(new data_holder< T >(val))
+        {
 
-		}
-		data_wrapper(const char* val) : content(NULL)
-		{
+        }
+        data_wrapper(const char* val) : content(NULL)
+        {
             if (val != NULL)
             {
                 content = new data_holder< std::string >(std::string(val));
             }
-		}
-		data_wrapper(const data_wrapper& rhs) : content(rhs.content != NULL ? rhs.content->clone() : NULL)
-		{
+        }
+        data_wrapper(const data_wrapper& rhs) : content(rhs.content != NULL ? rhs.content->clone() : NULL)
+        {
 
-		}
-		~data_wrapper()
-		{
-			delete content;
-			content = NULL;
-		}
+        }
+        ~data_wrapper()
+        {
+            delete content;
+            content = NULL;
+        }
 
-		// Basic operation
+        // Basic operation
 
-		const std::type_info& type_info() const
-		{
-			return content != NULL ? content->type_info() : typeid(void);
-		}
+        const std::type_info& type_info() const
+        {
+            return content != NULL ? content->type_info() : typeid(void);
+        }
 
-		data_wrapper& swap(data_wrapper &rhs)
-		{
-			std::swap(content, rhs.content);
-			return (*this);
-		}
+        data_wrapper& swap(data_wrapper& rhs)
+        {
+            std::swap(content, rhs.content);
+            return (*this);
+        }
 
-		data_wrapper& operator=(const data_wrapper &rhs)
-		{
-			return swap(data_wrapper(rhs));
-		}
+        data_wrapper& operator=(const data_wrapper& rhs)
+        {
+            data_wrapper dw(rhs);
+            return swap(dw);
+        }
 
-		bool valid() const
-		{
-			return ((this != NULL) && (content != NULL));
-		}
+        bool valid() const
+        {
+            return ((this != NULL) && (content != NULL));
+        }
 
-		// Template operation
+        // Template operation
 
-		template< typename T >
-		bool istype()
-		{
-			data_holder< T >* holder = dynamic_cast<data_holder< T >*>(content);
-			return (holder != NULL);
-		}
+        template< typename T >
+        bool istype()
+        {
+            data_holder< T >* holder = dynamic_cast<data_holder< T >*>(content);
+            return (holder != NULL);
+        }
 
-		template< typename T >
-		bool set(T t)
-		{
-			data_holder< T >* holder = dynamic_cast<data_holder< T >*>(content);
-			if (holder != NULL) { holder->set(t); return true;  }
-			else { return false;  }
-		}
-		bool set(const data_wrapper& t)
-		{
-			swap(data_wrapper(t));
-			return true;
-		}
+        template< typename T >
+        bool set(T t)
+        {
+            data_holder< T >* holder = dynamic_cast<data_holder< T >*>(content);
+            if (holder != NULL) { holder->set(t); return true; }
+            else { return false; }
+        }
+        bool set(const data_wrapper& t)
+        {
+            data_wrapper dw(t);
+            swap(dw);
+            return true;
+        }
         template< typename T >
         bool check(const T& expect)
         {
@@ -138,42 +144,42 @@ namespace dw {
             return ret;
         }
 
-		template< typename T >
-		T* pointer_as()
-		{
-			//if (typeid(T) == typeid(const char*))
-			//{
-			//	std::string* val = pointer_as< std::string >();
-			//	_content = (val != NULL ? val->c_str() : NULL);
-			//	return (T*)&_content;
-			//}
+        template< typename T >
+        T* pointer_as()
+        {
+            //if (typeid(T) == typeid(const char*))
+            //{
+            //	std::string* val = pointer_as< std::string >();
+            //	_content = (val != NULL ? val->c_str() : NULL);
+            //	return (T*)&_content;
+            //}
 
-			//if (content != NULL)
-			//{
-			//	const char* content_type = content->type_info().name();
-			//	const char* function_type = typeid(T).name();
-			//	(void)(content_type, function_type, "For debug.");
-			//}
+            //if (content != NULL)
+            //{
+            //	const char* content_type = content->type_info().name();
+            //	const char* function_type = typeid(T).name();
+            //	(void)(content_type, function_type, "For debug.");
+            //}
 
-			return ((content != NULL) && (content->type_info() == typeid(T))) ?
-				&(static_cast<data_holder< T >*>(content))->value : NULL;
-		}
+            return ((content != NULL) && (content->type_info() == typeid(T))) ?
+                &(static_cast<data_holder< T >*>(content))->value : NULL;
+        }
 
-		template< typename T >
-		T value_as()
-		{
-			return value_as(T());
-		}
+        template< typename T >
+        T value_as()
+        {
+            return value_as(T());
+        }
 
-		template< typename T >
-		T value_as(T default_val)
-		{
-			T* val = pointer_as< T >();
-			return (val != NULL ? (*val) : default_val);
-		}
-	};
+        template< typename T >
+        T value_as(T default_val)
+        {
+            T* val = pointer_as< T >();
+            return (val != NULL ? (*val) : default_val);
+        }
+    };
 
-	typedef data_wrapper any;
+    typedef data_wrapper any;
 
 }
 
--- Dataset/dataset.h
+++ Dataset/dataset.h
@@ -1,27 +1,31 @@
 #ifndef _DATASET_H
 #define _DATASET_H
 
+#pragma warning(push, 0)
 #include <map>
 #include <list>
 #include <tuple>
 #include <deque>
 #include <vector>
 #include <string>
+#pragma warning(pop)
+
+#pragma warning(disable: 4710)
 
 #include "data_wrapper.h"
 
 typedef std::map< std::string, dw::any > DATADICT;
-typedef std::deque< std::tuple< std::string , dw::any > > DATALIST;
+typedef std::deque< std::tuple< std::string, dw::any > > DATALIST;
 
 class IDataObserver
 {
 public:
-	IDataObserver() { }
-	virtual ~IDataObserver() { }
+    IDataObserver();
+    virtual ~IDataObserver();
 
-	virtual void onFirstData(std::string key, const dw::any& value) = 0;
-	virtual void onDataChanged(std::string key, const dw::any& value) = 0;
-	virtual void onBatchDataChanged(const DATALIST& values) = 0;
+    virtual void onFirstData(std::string key, const dw::any& value) = 0;
+    virtual void onDataChanged(std::string key, const dw::any& value) = 0;
+    virtual void onBatchDataChanged(const DATALIST& values) = 0;
 };
 
 
@@ -33,53 +37,56 @@ typedef std::map< IDataObserver*, DATALIST > UPDATESDICT;
 class ObserverOrganizer
 {
 protected:
-	UPDATESDICT m_updatesDict;						// Chache the updates for each ob
-	OBSERVERDICT m_observerDict;					// Store the observer for each data
+    UPDATESDICT m_updatesDict;						// Chache the updates for each ob
+    OBSERVERDICT m_observerDict;					// Store the observer for each data
 
-	OBSERVERLIST m_observerList;					// Store the observers, unique.
-	std::deque< std::string > m_observedList;
+    OBSERVERLIST m_observerList;					// Store the observers, unique.
+    std::deque< std::string > m_observedList;
 
 public:
-	ObserverOrganizer();
-	~ObserverOrganizer();
+    ObserverOrganizer();
+    ~ObserverOrganizer();
 
-	// Return false if the observer already exists on this key.
-	bool addObserverTo(IDataObserver* ob, const std::string& key);
-	void removeObserver(IDataObserver* ob);
-	void destroyObservers();
+    // Return false if the observer already exists on this key.
+    bool addObserverTo(IDataObserver* ob, const std::string& key);
+    void removeObserver(IDataObserver* ob);
+    void destroyObservers();
 
-	void addUpdate(const std::string& key, const dw::any& value);
+    void addUpdate(const std::string& key, const dw::any& value);
 
-	void flushUpdatesToObservers();
+    void flushUpdatesToObservers();
 
 protected:
-	bool addObserverForKey(const std::string& key, IDataObserver* ob);
-	void removeObserverForKey(const std::string& key, IDataObserver* ob);
+    bool addObserverForKey(const std::string& key, IDataObserver* ob);
+    void removeObserverForKey(const std::string& key, IDataObserver* ob);
 
-	DATALIST* findCreateObserverDataList(IDataObserver* ob);
-	std::deque< IDataObserver* >* findObserverQueue(const std::string& key);
+    DATALIST* findCreateObserverDataList(IDataObserver* ob);
+    std::deque< IDataObserver* >* findObserverQueue(const std::string& key);
 };
 
 
 class Dataset
 {
 protected:
-	typedef std::map< std::string, dw::any* > DATAPTDICT;
+    typedef std::map< std::string, dw::any* > DATAPTDICT;
 protected:
-	bool m_lockType;
-	DATAPTDICT m_dataDict;
-	ObserverOrganizer m_obOrganizer;
+    bool m_lockType;
+    DATAPTDICT m_dataDict;
+    ObserverOrganizer m_obOrganizer;
 
 public:
-	Dataset();
-	~Dataset();
+    Dataset();
+    ~Dataset();
 
-	void clear();
-	void lockType(bool lock);
+    void clear();
+    void lockType(bool lock);
 
-	dw::any get(std::string key) const;
-	bool get(std::string key, dw::any& value) const;
-	bool set(std::string key, const dw::any& value);
+    dw::any get(std::string key) const;
+    bool get(std::string key, dw::any& value) const;
+    bool set(std::string key, const dw::any& value);
+
+    DATADICT gets(std::vector< std::string > keys);
+    void sets(const DATADICT& values);
 
     template< typename T >
     bool check(std::string key, const T& expect) const
@@ -89,30 +96,21 @@ public:
         ret = ret && value.check< T >(expect);
         return ret;
     }
-    bool check(std::string key, const char* expect) const
-    {
-        dw::any value;
-        bool ret = get(key, value);
-        ret = ret && value.check(expect);
-        return ret;
-    }
+    bool check(std::string key, const char* expect) const;
 
-	DATADICT gets(std::vector< std::string > keys);
-	void sets(const DATADICT& values);
-
-	bool addDataObserver(std::string key, IDataObserver* ob);
-	void removeDataObserver(IDataObserver* ob);
+    bool addDataObserver(std::string key, IDataObserver* ob);
+    void removeDataObserver(IDataObserver* ob);
 
 protected:
-	bool requestDataAccess() const;
-	bool releaseDataAccess() const;
+    bool requestDataAccess() const;
+    bool releaseDataAccess() const;
 
-	void addUpdate(const std::string& key, const dw::any& value);
-	void commitUpdates();
+    void addUpdate(const std::string& key, const dw::any& value);
+    void commitUpdates();
 
-	dw::any* find(const std::string&) const;
-	bool update(const std::string&, const dw::any& value);
-	dw::any* create(const std::string& key, const dw::any& value);
+    dw::any* find(const std::string&) const;
+    bool update(const std::string&, const dw::any& value);
+    dw::any* create(const std::string& key, const dw::any& value);
 };
 
 #endif // _DATASET_H
